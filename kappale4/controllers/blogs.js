8281a668eb61a08cc612aa6f0bdb0b6e676ad987
PietariImmonen <pietari.immonen@gmail.com>
2022-05-31 12:27:11 +0300

    4.17

--- kappale4/controllers/blogs.js
+++ kappale4/controllers/blogs.js
@@ -1,10 +1,11 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const { nonExistingId } = require('../tests/test_helper')
+const User = require('../models/user')
 
 
 blogsRouter.get('/', async(request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate("user", {username:1, name:1})
     response.json(blogs)
   })
 
@@ -17,14 +18,19 @@ blogsRouter.post('/', async (request, response) => {
       like = body.likes
     }
 
+    const user = await User.findById(body.userId)
+
 
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
-      likes: like
+      likes: like,
+      user: user.id
     })
       const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog.id)
+      await user.save()
       response.status(201).json(savedBlog)
 })
 
