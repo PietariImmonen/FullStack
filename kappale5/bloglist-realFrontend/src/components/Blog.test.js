095ffb929e2da6e9a391b24655ae258c59a61467
PietariImmonen <pietari.immonen@gmail.com>
2022-06-02 17:40:02 +0300

    5.15

--- kappale5/bloglist-realFrontend/src/components/Blog.test.js
+++ kappale5/bloglist-realFrontend/src/components/Blog.test.js
@@ -26,7 +26,7 @@ test('renders title and author but not url and likes', () => {
   expect(urlNlikes).toBe(null)
 })
 
-test('clicking the button calls event handler once', async () => {
+test('when clicked shows likes and url', async () => {
   const blog = {
     title: 'Component testing is done with react-testing-library',
     author: 'Puppe Bubbe',
@@ -39,4 +39,23 @@ test('clicking the button calls event handler once', async () => {
   const button = screen.getByText('View')
   await user.click(button)
   expect(container).toHaveTextContent(blog.url)
+})
+
+test('clicking the button calls event handler once', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Puppe Bubbe',
+    url: 'https://Masa.com',
+    likes: 10
+  }
+  const mockHandler2 = jest.fn()
+  const { container } = render(<Blog blog={blog} addLike={mockHandler2}/>)
+  const user = userEvent.setup()
+  const button1 = screen.getByText('View')
+  await user.click(button1)
+  expect(container).toHaveTextContent(blog.likes)
+  const button2 = screen.getByText('Like')
+  await user.click(button2)
+  await user.click(button2)
+  expect(mockHandler2.mock.calls).toHaveLength(2)
 })
