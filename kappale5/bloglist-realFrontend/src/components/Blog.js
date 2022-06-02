0c001d0c1a62850eadc6a2415ee80b472a303b70
PietariImmonen <pietari.immonen@gmail.com>
2022-06-02 11:45:31 +0300

    5.7

--- kappale5/bloglist-realFrontend/src/components/Blog.js
+++ kappale5/bloglist-realFrontend/src/components/Blog.js
@@ -1,7 +1,31 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+import "./blog.css"
+
+const Blog = ({blog}) => {
+
+  const [isShow, setIsShow] = useState(false)
+  console.log(isShow)
+
+  const toggle = () => {
+    setIsShow(!isShow)
+  }
+
+
+  return(
+  <div className="blog">
+    <div>
+      {blog.title}
+      <button onClick={toggle}>View</button>
+      {isShow && 
+        <div>
+          <p>{blog.author}</p>
+          <p>{blog.url}</p>
+          <p>likes: {blog.likes} <button>like</button></p>
+        </div>
+      }
+    </div>
   </div>  
-)
+  )
+}
 
 export default Blog
